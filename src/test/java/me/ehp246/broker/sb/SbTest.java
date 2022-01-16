4ea8beefb83f558e6bb01c16d33e95ca9177bcc6
Lei Yang <ehp246@yahoo.com>
2022-01-16 13:53:26 -0500

    Ready for release.

--- src/test/java/me/ehp246/broker/sb/SbTest.java
+++ src/test/java/me/ehp246/broker/sb/SbTest.java
@@ -2,12 +2,22 @@ package me.ehp246.broker.sb;
 
 import java.util.stream.IntStream;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+import me.ehp246.aufjms.api.jms.AufJmsContext;
 
 /**
  * @author Lei Yang
@@ -15,9 +25,29 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
  */
 @SpringBootTest(classes = { AppConfig.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
 @EnabledIfSystemProperty(named = "me.ehp246.broker.sb", matches = "true")
+@TestInstance(Lifecycle.PER_CLASS)
 class SbTest {
     @Autowired
+    private ConnectionFactory cf;
+    private Session session;
+
+    @Autowired
     private ToInbox toInbox;
+
+    @BeforeAll
+    void setup() throws JMSException {
+        session = cf.createConnection().createSession();
+    }
+
+    @BeforeEach
+    void reset() {
+        AufJmsContext.clearSession();
+    }
+
+    @AfterAll
+    void teardown() throws JMSException {
+        session.close();
+    }
 
     @Test
     void send_001() {
@@ -37,4 +67,12 @@ class SbTest {
             Thread.sleep(1 * 60000);
         }
     }
+
+    @Test
+    void send_004() throws JMSException {
+        AufJmsContext.set(session);
+
+        IntStream.range(0, 300).forEach(toInbox::ping);
+    }
+
 }
