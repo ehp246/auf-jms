e4e230ee7e54730f4c410b31e20d4f357820f9a1
Lei Yang <ehp246@yahoo.com>
2022-07-25 14:00:02 -0400

    Disabled bulk test.

---
+++ src/test/java/me/ehp246/test/bulk/AppConfig.java
@@ -0,0 +1,20 @@
+package me.ehp246.test.bulk;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import me.ehp246.aufjms.api.annotation.EnableByJms;
+import me.ehp246.aufjms.util.EmbeddedArtemisConfig;
+
+/**
+ * @author Lei Yang
+ *
+ */
+@EnableByJms
+@Import({ EmbeddedArtemisConfig.class })
+@SpringBootApplication
+@ActiveProfiles("local")
+class AppConfig {
+
+}
