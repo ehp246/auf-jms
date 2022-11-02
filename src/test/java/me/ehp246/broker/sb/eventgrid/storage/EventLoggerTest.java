d9ba6d4b788c6c74045f20e2b34372f47f0c44a5
Lei Yang <ehp246@yahoo.com>
2022-11-02 15:39:11 -0400

    Blob event tests

---
+++ src/test/java/me/ehp246/broker/sb/eventgrid/storage/EventLoggerTest.java
@@ -0,0 +1,27 @@
+package me.ehp246.broker.sb.eventgrid.storage;
+
+import java.util.concurrent.ExecutionException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author Lei Yang
+ *
+ */
+@SpringBootTest(classes = { AppConfig.class }, webEnvironment = WebEnvironment.NONE)
+@EnabledIfSystemProperty(named = "me.ehp246.broker.sb", matches = "true")
+@ActiveProfiles("local")
+class EventLoggerTest {
+    @Autowired
+    private EventLogger event;
+
+    @Test
+    void take_01() throws InterruptedException, ExecutionException {
+        event.take();
+    }
+}
