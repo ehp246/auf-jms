c74a8e794ca33e77f52eabd5e55db71c90cabedc
Lei Yang <ehp246@yahoo.com>
2022-11-07 11:03:59 -0500

    Just needed the sub.

--- src/test/java/me/ehp246/aufjms/integration/endpoint/failedmsg/AppConfig.java
+++ src/test/java/me/ehp246/aufjms/integration/endpoint/failedmsg/AppConfig.java
@@ -10,8 +10,10 @@ import me.ehp246.aufjms.api.annotation.EnableByJms;
 import me.ehp246.aufjms.api.annotation.EnableForJms;
 import me.ehp246.aufjms.api.annotation.EnableForJms.Inbound;
 import me.ehp246.aufjms.api.annotation.EnableForJms.Inbound.From;
+import me.ehp246.aufjms.api.annotation.EnableForJms.Inbound.From.Sub;
 import me.ehp246.aufjms.api.endpoint.InvocationListener.OnFailed;
 import me.ehp246.aufjms.api.endpoint.Invoked.Failed;
+import me.ehp246.aufjms.api.jms.DestinationType;
 import me.ehp246.aufjms.api.jms.JmsMsg;
 import me.ehp246.aufjms.integration.endpoint.failedmsg.dltopic.OnDlqMsg;
 import me.ehp246.aufjms.integration.endpoint.failedmsg.failed.FailMsg;
@@ -26,7 +28,7 @@ import me.ehp246.aufjms.util.EmbeddedArtemisConfig;
 @EnableForJms({
         @Inbound(value = @From("q1"), scan = FailMsg.class, invocationListener = "consumer1"),
         @Inbound(value = @From("q2"), scan = FailMsg.class, invocationListener = "consumer2"),
-        @Inbound(value = @From("dlq"), scan = OnDlqMsg.class) })
+        @Inbound(value = @From(value = "dlq", type = DestinationType.TOPIC, sub = @Sub("s1")), scan = OnDlqMsg.class) })
 @Import(EmbeddedArtemisConfig.class)
 class AppConfig {
     public CompletableFuture<Failed> conRef1 = new CompletableFuture<>();
--- src/test/java/me/ehp246/aufjms/integration/endpoint/failedmsg/dltopic/SendToDlTopic.java
+++ src/test/java/me/ehp246/aufjms/integration/endpoint/failedmsg/dltopic/SendToDlTopic.java
@@ -5,12 +5,13 @@ import me.ehp246.aufjms.api.annotation.ByJms.To;
 import me.ehp246.aufjms.api.annotation.OfCorrelationId;
 import me.ehp246.aufjms.api.endpoint.InvocationListener.OnFailed;
 import me.ehp246.aufjms.api.endpoint.Invoked.Failed;
+import me.ehp246.aufjms.api.jms.DestinationType;
 
 /**
  * @author Lei Yang
  *
  */
-@ByJms(value = @To(value = "dlq"), name = "consumer2")
+@ByJms(value = @To(value = "dlq", type = DestinationType.TOPIC), name = "consumer2")
 public interface SendToDlTopic extends OnFailed {
     void send(@OfCorrelationId String id);
 
