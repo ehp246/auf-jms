cea279d6b0ad97b5142aabd1aae951f736ea103a
Lei Yang <ehp246@yahoo.com>
2022-11-07 16:56:36 -0500

    New incompatible property name for the dispatch logger.

--- src/main/java/me/ehp246/aufjms/core/configuration/AufJmsConstants.java
+++ src/main/java/me/ehp246/aufjms/core/configuration/AufJmsConstants.java
@@ -7,8 +7,8 @@ package me.ehp246.aufjms.core.configuration;
 public final class AufJmsConstants {
     public static final String TIMEOUT = "me.ehp246.aufjms.timeout";
     public static final String TTL = "me.ehp246.aufjms.ttl";
-    public static final String POOL_SIZE = "me.ehp246.aufjms.executor.poolSize";
-    public static final String DISPATCH_LOGTER = "me.ehp246.aufjms.dispatchLogger";
+    public static final String POOL_SIZE = "me.ehp246.aufjms.executor.pool-size";
+    public static final String DISPATCH_LOGTER = "me.ehp246.aufjms.dispatch-logger.enabled";
 
     public static final long TIMEOUT_DEFAULT = 30000;
 
