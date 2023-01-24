2aaec9269f91ccbf04b4932f0027dc42cb69f449
Lei Yang <ehp246@yahoo.com>
2023-01-24 13:44:36 -0500

    Adjusting SB tests

--- src/test/java/me/ehp246/test/asb/sub/AppConfig.java
+++ src/test/java/me/ehp246/test/asb/sub/AppConfig.java
@@ -10,7 +10,6 @@ import me.ehp246.aufjms.api.annotation.EnableForJms.Inbound.From;
 import me.ehp246.aufjms.api.annotation.EnableForJms.Inbound.From.Sub;
 import me.ehp246.aufjms.api.jms.DestinationType;
 import me.ehp246.test.asb.SimpleServiceBusConfig;
-import me.ehp246.test.asb.sub.localevent.LocalEvent;
 
 /**
  * @author Lei Yang
@@ -18,9 +17,7 @@ import me.ehp246.test.asb.sub.localevent.LocalEvent;
  */
 @EnableByJms
 @EnableForJms({
-        @Inbound(value = @From(value = "auf-jms.echo.event", type = DestinationType.TOPIC, sub = @Sub("sub1")), autoStartup = "false"),
-        @Inbound(value = @From(value = "auf-jms.echo.event", type = DestinationType.TOPIC, sub = @Sub("sub2$$D")), autoStartup = "false"),
-        @Inbound(value = @From(value = "auf-jms.echo.event", type = DestinationType.TOPIC, sub = @Sub(value = "sub1", shared = false, durable = false)), autoStartup = "true", scan = LocalEvent.class) })
+        @Inbound(value = @From(value = "auf-jms.echo.event", type = DestinationType.TOPIC, sub = @Sub("simple-sub")), autoStartup = "false") })
 @Import({ SimpleServiceBusConfig.class })
 @SpringBootApplication
 class AppConfig {
--- src/test/java/me/ehp246/test/asb/sub/ToEchoTopic.java
+++ src/test/java/me/ehp246/test/asb/sub/ToEchoTopic.java
@@ -2,12 +2,13 @@ package me.ehp246.test.asb.sub;
 
 import me.ehp246.aufjms.api.annotation.ByJms;
 import me.ehp246.aufjms.api.annotation.ByJms.To;
+import me.ehp246.aufjms.api.jms.DestinationType;
 
 /**
  * @author Lei Yang
  *
  */
-@ByJms(@To("auf-jms.echo.event"))
+@ByJms(value = @To(value = "auf-jms.echo.event", type = DestinationType.TOPIC))
 interface ToEchoTopic {
     void echo(int i);
 }
