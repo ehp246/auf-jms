3766445f4b717a9c88a5475e75a13ca0f69fb5ea
Lei Yang <ehp246@yahoo.com>
2023-01-25 17:02:33 -0500

    Allowing dynamic registration.

--- src/test/java/me/ehp246/test/app/inbound/registry/RegistryTest.java
+++ src/test/java/me/ehp246/test/app/inbound/registry/RegistryTest.java
@@ -1,10 +1,15 @@
 package me.ehp246.test.app.inbound.registry;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import me.ehp246.aufjms.api.inbound.InboundEndpoint;
+import me.ehp246.aufjms.api.inbound.InvocableTypeDefinition;
+import me.ehp246.aufjms.core.reflection.ReflectedType;
 import me.ehp246.test.mock.action.OnMsg;
 
 /**
@@ -39,6 +44,32 @@ class RegistryTest {
 
         Assertions.assertEquals(0,
                 appCtx.getBean("inboundEndpoint-1", InboundEndpoint.class).typeRegistry().registered().size());
+
+        appCtx.close();
+    }
+
+    @Test
+    void test_03() {
+        final var appCtx = new AnnotationConfigApplicationContext(AppConfig.Config01.class);
+
+        final var inbound = appCtx.getBean("inboundEndpoint-0", InboundEndpoint.class);
+
+        final var typeRegistry = inbound.typeRegistry();
+
+        Assertions.assertEquals(0, typeRegistry.registered().size());
+
+        final var method = ReflectedType.reflect(String.class).findMethod("toString");
+
+        typeRegistry.register(
+                new InvocableTypeDefinition(Set.of("OnMsg"), String.class, Map.of("", method, "toString", method)));
+
+        Assertions.assertEquals(1, typeRegistry.registered().size());
+
+        final var onMsg = typeRegistry.registered().get("OnMsg");
+
+        Assertions.assertEquals(String.class, onMsg.type());
+        Assertions.assertEquals(method, onMsg.methods().get(""));
+        Assertions.assertEquals(method, onMsg.methods().get("toString"));
 
         appCtx.close();
     }
