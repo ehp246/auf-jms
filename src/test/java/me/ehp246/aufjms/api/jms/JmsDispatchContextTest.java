fbf31923c30d4ade7e5b74ad5ff308ffda753c18
Lei Yang <ehp246@yahoo.com>
2023-02-28 12:10:30 -0500

    New method names.

--- src/test/java/me/ehp246/aufjms/api/jms/JmsDispatchContextTest.java
+++ src/test/java/me/ehp246/aufjms/api/jms/JmsDispatchContextTest.java
@@ -1,5 +1,10 @@
 package me.ehp246.aufjms.api.jms;
 
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,15 +16,58 @@ import jakarta.jms.JMSContext;
  *
  */
 class JmsDispatchContextTest {
+    @AfterEach
+    void clear() {
+        JmsDispatchContext.remove();
+    }
+
     @Test
     void clear_01() throws Exception {
         final var mock = Mockito.mock(JMSContext.class);
 
-        JmsDispatchContext.set(mock).close();
+        JmsDispatchContext.setJmsContext(mock).close();
 
         Mockito.verify(mock).close();
 
-        Assertions.assertEquals(null, JmsDispatchContext.getJmsContext());
+        Assertions.assertEquals(null, JmsDispatchContext.jmsContext());
     }
 
+    @Test
+    void property_01() throws Exception {
+        final Map<String, Object> map = Map.of("", "");
+
+        JmsDispatchContext.setProperties(map);
+
+        Assertions.assertEquals(map, JmsDispatchContext.properties());
+
+        JmsDispatchContext.remove();
+
+        Assertions.assertEquals(null, JmsDispatchContext.jmsContext());
+    }
+
+    @Test
+    void property_02() throws Exception {
+        final Map<String, Object> map = Map.of("", "");
+
+        JmsDispatchContext.setProperties(map);
+
+        final var exe = Executors.newSingleThreadExecutor();
+        final var ref = new AtomicReference<Map<String, ?>>();
+
+        exe.execute(() -> {
+            ref.set(JmsDispatchContext.properties());
+        });
+
+        Assertions.assertEquals(null, ref.get());
+    }
+
+    @Test
+    void property_03() throws Exception {
+        final Map<String, Object> map = Map.of("", "");
+
+        try (final var closeable = JmsDispatchContext.setProperties(map)) {
+        }
+
+        Assertions.assertEquals(0, JmsDispatchContext.properties().size());
+    }
 }
